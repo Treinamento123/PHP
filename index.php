28ec357f332615d9b9d89c582c6516953801aa77
Miguelito Fonseca <jboladooooo@gmail.com>
2018-10-04 16:15:24 -0300

    Treinando mais algumas coisas

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
 echo "Hello World!";
+echo "<br/><br/>";
+
+$nome = "Miguelito";
+$sobrenome = "Fonseca";
+$nomeCompleto = $nome . " " . $sobrenome;
+
+echo $nomeCompleto;
+
+$frutas = array("Morango", "Laranja", "Uva");
+
+echo "<br/><br/>";
+echo $frutas[2];
 
 ?>
